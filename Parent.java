529fb861170fbbf18cf2859adde03ad3f86b51b5
priyankavasani30 <priyankavasani428@gmail.com>
2024-06-09 16:56:41 +0530

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,10 @@
+package convarianttype;
+
+public class Parent {
+	public Integer display()
+	{
+		Object o;
+		System.out.println("I am parent");
+		return o;
+	}
+}
---
+++ child.java
@@ -0,0 +1,11 @@
+package convarianttype;
+
+public class child extends Parent{
+
+	public Integer display()
+	{
+		int k;
+		System.out.println("I am child");
+		return k;
+	}
+}
